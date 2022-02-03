1b137cda4bb844bac8782cfbefd84df0b424f544
saradamian <damian.podareanu@surfsara.nl>
2022-02-03 12:15:59 +0100

    update reservation name and add mem constraint

--- notebooks/parallel_ml/submit_script.sh
+++ notebooks/parallel_ml/submit_script.sh
@@ -3,15 +3,16 @@
 #SBATCH -o distrib_test_out.txt
 #SBATCH -e distrib_test_err.txt
 
-#SBATCH -t 0:30:00
+#SBATCH -t 0:10:00
 
 #SBATCH --partition gpu_shared_course
-#SBATCH --reservation jupyterhub_course_jhlsrf011_2022-02-03
+#SBATCH --reservation jhlsrf011_overlap
 
 #SBATCH --nodes 1
 #SBATCH --gpus 2
 #SBATCH --ntasks-per-node=2
 
+#SBATCH --mem=64G
 
 module load 2021
 module load PyTorch/1.10.0-foss-2021a-CUDA-11.3.1 torchvision/0.11.1-foss-2021a-CUDA-11.3.1
